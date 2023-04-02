1f9a45b830a9419233d3764834ac77af187bbc9e
Dmitry <Dmitry@DESKTOP-QSPLIG5>
2023-04-02 11:00:02 +0300

    change1

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -1,45 +1,91 @@
 ﻿#include<iostream>
 using namespace std;
+void FillRand(int arr[], int n);
+void Print(int arr[], int n);
 
-int* push_Back (int arr[], int n, int value)
+int* push_Back(int arr[], int& n, int value);
+int* push_front(int arr[], int& n, int value);
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
+
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	int n = 0;
+	int n;
 	int value;
 	cout << "Введите размер массива: "; cin >> n;
-	int* arr = new int[n + 1];
+	int* arr = new int[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
 	cout << "Введите добавляемое значение: "; cin >> value;
-	int* buffer = new int[n + 1];
-	//int x = 7;//добавляемое значение
+	arr = push_Back(arr, n, value);
+	Print(arr, n);
+
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
+	delete[] arr;
 }
-	int* push_Back(int arr[], int n, int value)
+int* push_Back(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	arr[n] = value;
+	n++;
+	return arr;
+}
+int* push_front(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete arr;
+	arr = buffer;
+	arr[0] = value;
+	n++;
+	return buffer;
+}
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++) buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i <n; i++)buffer[i] = arr[i+1];
+	delete[] arr;
+	return buffer;
+}
+void FillRand(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
-		
+
 	}
-	
+}
+void Print(int arr[], int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		cout << *(arr + i) << "\t";
 	}
 	cout << endl;
-	//int* newArr = new int[n + 1];
-	for (int i = 0; i < n; i++)
-	{
-		buffer[i] = arr[i];
-	}
-	//newArr[n] = x;
-	delete[] arr;
-	arr = buffer;
-	arr[n] = value;
-	n ++;
-	cout << "Массив с добавленным элементом: " << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	delete[] arr;
 }
